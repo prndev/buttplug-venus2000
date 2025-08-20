d6730700faa543cb4f6809e924ad16ee0d269b84
prndev <prndev@users.noreply.github.com>
2025-08-20 22:53:50 +0200

    vibration handling in separate class

--- lustbound.py
+++ lustbound.py
@@ -5,34 +5,82 @@ import time
 import collections
 from serial import Serial
 import argparse
+import collections
 
 def clamp(value):
     return min(1.0, max(0.0, value))
+
+Sample = collections.namedtuple('Sample', ['value', 'timestamp_ms'])
+
+class VibrationHandler:
+    def __init__(self, args, set_intensity_callback):
+        self.serial_read_timeout = None
+        self.samples:collections.deque[Sample] = collections.deque()
+        self.cycle_times_ms:collections.deque[int] = collections.deque()
+        self.falling_timestamp_ms = None
+        self.rising_timestamp_ms = None
+        self.set_intensity_callback = set_intensity_callback
+        self.args = args
+
+    def stop(self):
+        self.serial_read_timeout = None
+        self.samples.clear()
+        self.cycle_times_ms.clear()
+        self.falling_timestamp_ms = None
+        self.rising_timestamp_ms = None
+        self.set_intensity_callback(0.0)
+
+    def update(self, value, timestamp_ms):
+        self.serial_read_timeout = 0.1 # we are no longer "stopped by timeout"
+        sample = Sample(value, timestamp_ms)
+        # calculate average cycle time
+        avg_cycle_time = None
+        if (len(self.samples)):
+            if (self.samples[-1].value > value): # falling slope
+                if (self.falling_timestamp_ms and self.rising_timestamp_ms):
+                    if (self.falling_timestamp_ms < self.rising_timestamp_ms):
+                        self.cycle_times_ms.append(self.rising_timestamp_ms-self.falling_timestamp_ms)
+                self.falling_timestamp_ms = timestamp_ms
+            if (self.samples[-1].value < value):
+                if (self.falling_timestamp_ms and self.rising_timestamp_ms):
+                    if (self.rising_timestamp_ms < self.falling_timestamp_ms):
+                        self.cycle_times_ms.append(self.falling_timestamp_ms-self.rising_timestamp_ms)
+                self.rising_timestamp_ms = timestamp_ms
+            # limit amount of (half-)cycles to averare over
+            while(len(self.cycle_times_ms) > args.cycle_max_samples):
+                self.cycle_times_ms.popleft()
+            if (self.cycle_times_ms):
+                avg_cycle_time = sum(self.cycle_times_ms)/len(self.cycle_times_ms)
+        self.samples.append(sample)
+        
+        # remove old samples
+        while (self.samples[-1].timestamp_ms-self.samples[0].timestamp_ms > args.amplitude_sample_ms):
+            self.samples.popleft()
+        # get amplitude daftly
+        amplitude = max(self.samples).value - min(self.samples).value
+        
+        cycle_time = avg_cycle_time if avg_cycle_time else self.args.cycle_max_ms
+        cycle_intensity = clamp((self.args.cycle_max_ms+self.args.cycle_min_ms - cycle_time)/(self.args.cycle_max_ms-self.args.cycle_min_ms))
+        intensity = amplitude * cycle_intensity
+        set_intensity(intensity)
+
+        print(f"{amplitude:.2} {cycle_intensity:.2} → {intensity:.2}")
 
 async def handle_client(websocket, args, set_intensity):
     print("Client connected!")
     try:
         message_version = 2
         client_connect_timestamp_ms = time.time_ns() // 1_000_000
-        timeout = None
-        values = collections.deque()
-        cycle_times_ms = collections.deque()
-        falling_timestamp_ms = None
-        rising_timestamp_ms = None
+        vibration_handler = VibrationHandler(args, set_intensity)
         while True:
             message = None
             #async for message in websocket:
             try:
-                async with asyncio.timeout(timeout):
+                async with asyncio.timeout(vibration_handler.serial_read_timeout):
                     message = await websocket.recv()
             except TimeoutError:
                 print("Stop by timeout")
-                timeout = None
-                values.clear()
-                cycle_times_ms.clear()
-                falling_timestamp_ms = None
-                rising_timestamp_ms = None
-                set_intensity(0.0)
+                vibration_handler.stop()
             if (message):
                 message = json.loads(message)
                 message = message[0]
@@ -57,40 +105,7 @@ async def handle_client(websocket, args, set_intensity):
                 elif ("VibrateCmd" in message): # NOTE: only exists in message_version 2
                     value = message["VibrateCmd"]["Speeds"][0]["Speed"]
                     timestamp_ms = (time.time_ns() // 1_000_000) - client_connect_timestamp_ms
-                    #print(timestamp_ms, value)
-                    timeout = 0.1
-                    if (value > 0.001):
-                        # calculate average cycle time
-                        avg_cycle_time = None
-                        if (len(values)):
-                            if (values[-1][0] > value):
-                                if (falling_timestamp_ms and rising_timestamp_ms):
-                                    if (falling_timestamp_ms < rising_timestamp_ms):
-                                        cycle_times_ms.append(rising_timestamp_ms-falling_timestamp_ms)
-                                falling_timestamp_ms = timestamp_ms
-                            if (values[-1][0] < value):
-                                if (falling_timestamp_ms and rising_timestamp_ms):
-                                    if (rising_timestamp_ms < falling_timestamp_ms):
-                                        cycle_times_ms.append(falling_timestamp_ms-rising_timestamp_ms)
-                                rising_timestamp_ms = timestamp_ms
-                            # limit amount of (half-)cycles to averare over
-                            while(len(cycle_times_ms) > args.cycle_max_samples):
-                                cycle_times_ms.popleft()
-                            if (cycle_times_ms):
-                                avg_cycle_time = sum(cycle_times_ms)/len(cycle_times_ms)
-                        values.append((value, timestamp_ms))
-                        
-                        while (values[-1][1]-values[0][1] > args.amplitude_sample_ms):
-                            values.popleft()
-                        amplitude = max(values)[0]-min(values)[0]
-                        
-                        cycle_time = avg_cycle_time if avg_cycle_time else args.cycle_max_ms
-                        cycle_intensity = clamp((args.cycle_max_ms+args.cycle_min_ms - cycle_time)/(args.cycle_max_ms-args.cycle_min_ms))
-                        intensity = amplitude * cycle_intensity
-                        print(f"{amplitude:.2} {cycle_intensity:.2} → {intensity:.2}")
-                        set_intensity(intensity)
-                #elif ("RotateCmd" in message):
-                #    print("Got rotate:", message["VibrateCmd"]["Speeds"][0]["Speed"])
+                    vibration_handler.update(value, timestamp_ms)
                 print(">", response)
                 await websocket.send(json.dumps(response))
     except websockets.exceptions.ConnectionClosed:
